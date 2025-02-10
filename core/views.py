281f9809ec1bf648730403b417e994ec0baf888a
ibrahimkvlci <ibrahimkvlci@gmail.com>
2025-02-10 22:04:39 +0300

    Create new object includes top_movies list and their interactions

--- core/views.py
+++ core/views.py
@@ -3,6 +3,8 @@ from django.http import HttpResponse
 
 from external_api.moviedb_api import moviedb
 
+from movies.models import UserMovie
+
 # Create your views here.
 
 def index(request):
@@ -11,6 +13,8 @@ def index(request):
     except:
         top_movies=[]
 
+    top_movies_with_interactions=[{'top_movie':top_movie,'interactions':{'likes':UserMovie.objects.get_likes(top_movie['id']),'favorites':UserMovie.objects.get_favorites(top_movie['id'])}} for top_movie in top_movies]
+
     return render(request,'core/index.html',{
-        'top_movies':top_movies,
+        'top_movies':top_movies_with_interactions,
     })
