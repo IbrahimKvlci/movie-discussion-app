12f86015e9e3af3ea0c1e6de2dcba9a6a5425324
ibrahimkvlci <ibrahimkvlci@gmail.com>
2025-02-10 18:10:22 +0300

    Create conversationrooms models

--- conversationrooms/models.py
+++ conversationrooms/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class ConversationRoom(models.Model):
+    movie_id=models.IntegerField()
+    members=models.ManyToManyField(User,related_name='joined_conversation_rooms')
+    created_by=models.ForeignKey(User,on_delete=models.CASCADE,related_name='created_conversation_rooms')
+    created_at=models.DateField(auto_now_add=True)
+    modified_at=models.DateField(auto_now=True)
+
+    class Meta:
+        ordering=('-modified_at',)
+
+class ConversationRoomMessage(models.Model):
+    conversation_room=models.ForeignKey(ConversationRoom,on_delete=models.CASCADE,related_name='conversation_room_messages')
+    member=models.ForeignKey(User,on_delete=models.CASCADE,related_name='sent_conversation_room_messages')
+    content=models.TextField(max_length=300)
+    created_at=models.DateField(auto_now_add=True)
