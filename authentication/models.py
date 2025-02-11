2cbb5786fec6ed57b0fd66152b6d882762e8331f
ibrahimkvlci <ibrahimkvlci@gmail.com>
2025-02-11 15:35:14 +0300

    Refactor Profile model to use user as primary key and update migrations

--- authentication/models.py
+++ authentication/models.py
@@ -4,6 +4,6 @@ from django.db import models
 # Create your models here.
 
 class Profile(models.Model):
-    user=models.OneToOneField(User,on_delete=models.CASCADE)
+    user=models.OneToOneField(User,on_delete=models.CASCADE,primary_key=True)
     profile_image=models.ImageField(upload_to='profile_images/',blank=True,null=True)
     
